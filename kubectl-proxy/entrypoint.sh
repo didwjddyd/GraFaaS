0f5e5d40dc753bf9b093be109289a402d9521655
JJhwan7099 <ghks7099@naver.com>
2024-10-14 23:46:27 +0900

    .

--- kubectl-proxy/entrypoint.sh
+++ kubectl-proxy/entrypoint.sh
@@ -26,7 +26,7 @@ start_strace() {
 
     # 컨테이너 이름에서 마지막 두 단어를 제거
     LOG_NAME=$(echo "$CONTAINER_NAME" | awk -F- '{OFS="-"; NF-=2; print}')
-
+    
     strace -ttt -q -o /tmp/${CONTAINER_NAME}/${LOG_NAME}_syscalls.log -e trace=execve,fork,clone,open,socket,bind,listen,accept4,connect,sendto,recvfrom,chmod,chown,access,unlink,unlinkat -ff -p $TARGET_PID &
     STRACE_PID=$!
     echo -e "Started strace for *$CONTAINER_NAME* with PID: $STRACE_PID at PID: $TARGET_PID \n"
@@ -37,38 +37,60 @@ monitor_processes() {
     # 이미 strace를 시작한 PID를 저장할 배열
     declare -a traced_pids
     declare -a dir_names
+    touch /tmp/test.txt
     # 무한 루프를 돌며 프로세스를 감시
     while true; do
-        # index.js 프로세스의 PID 목록 추출
-	fwatchdog_pids=$(pgrep -f "fwatchdog")
+        # fwatchdog와 index.js 프로세스의 PID 목록 추출
+        fwatchdog_pids=$(pgrep -f "fwatchdog")
         pids=$(pgrep -f "index")
 
-	if [ -z "$fwatchdog_pids" ]; then
-	    sleep 0.05
-	    continue
-	fi
-	
-	for fwatchdog_pid in $fwatchdog_pids; do
-	    func_name=$(cat /proc/$fwatchdog_pid/environ | tr '\0' '\n' | grep '^HOSTNAME=' | cut -d '=' -f 2)
-	    if [ -z "$func_name" ]; then
-		echo "Can not find function name to mkdir in PID $fwatchdog_pid."
-	        continue
-	    fi
-	    if [[ ! " ${dir_names[@]} " =~ " $func_name " ]]; then
-		mkdir -p /tmp/$func_name
-		echo "$fwatchdog_pid" > /tmp/$func_name/fwatchdog_pid
-	        dir_names+=("$func_name")
-		echo -e "\nDetected fwatchdog process with PID $fwatchdog_pid for *$func_name*. Make dir complete."
-	    fi
-	done
+        if [ -z "$fwatchdog_pids" ]; then
+            sleep 0.05
+            continue
+        fi
+        
+        for fwatchdog_pid in $fwatchdog_pids; do
+            func_name=$(cat /proc/$fwatchdog_pid/environ | tr '\0' '\n' | grep '^HOSTNAME=' | cut -d '=' -f 2)
+            if [ -z "$func_name" ]; then
+                echo "Can not find function name to mkdir in PID $fwatchdog_pid."
+                continue
+            fi
+            
+            if [[ ! " ${dir_names[@]} " =~ " $func_name " ]]; then
+                LOG_DIR="/tmp/${func_name}"
+                OUTPUT_FILE="/tmp/test.txt"
+                mkdir -p "$LOG_DIR"
+                
+                LOG_NAME=$(echo "$func_name" | awk -F- '{OFS="-"; NF-=2; print}')
+                
+                {
+                    # inotifywait로 새로운 로그 파일 생성 감지
+                    inotifywait -m -e create "$LOG_DIR" --format '%f' |
+                    while read NEW_LOG_FILE; do
+                        echo "New log file detected: $LOG_DIR/$NEW_LOG_FILE"
+                        if [[ $NEW_LOG_FILE == ${LOG_NAME}_syscalls.log.* ]]; then
+                            # tail -f로 새로운 로그 파일을 모니터링합니다.
+                            {
+                                tail -f "$LOG_DIR/$NEW_LOG_FILE" | awk -v log_dir="$LOG_DIR" -v output_file="/tmp/test.txt" -f /tmp/Catch_clone.awk
+                            } &
+
+                        fi
+                    done
+                } &
+
+                echo "$fwatchdog_pid" > "$LOG_DIR/fwatchdog_pid"
+                dir_names+=("$func_name")
+                echo -e "\nDetected fwatchdog process with PID $fwatchdog_pid for *$func_name*. Make dir complete."
+            fi
+        done
+        
         # PID가 없으면 계속 진행
         if [ -z "$pids" ]; then
             sleep 0.05
             continue
         fi
+        
         for pid in $pids; do
-            # HOSTNAME 환경 변수 추출
-	    #function_name=$(cat /proc/$fwatchdog_pid/environ | tr '\0' '\n' | grep '^HOSTNAME=' | cut -d '=' -f 2)
             func_name=$(cat /proc/$pid/environ | tr '\0' '\n' | grep '^HOSTNAME=' | cut -d '=' -f 2)
 
             # func_name이 없으면 무시하고 다음 PID로 이동
@@ -81,11 +103,10 @@ monitor_processes() {
             STRACE_PID=$(pgrep -f "strace -ttt -p $pid")
             if [ -z "$STRACE_PID" ]; then
                 # strace가 실행 중이지 않은 경우
-                if [[ ! " ${traced_pids[@]} " =~ " $pid " && -d "/tmp/$func_name" ]]; then
+                if [[ ! " ${traced_pids[@]} " =~ " $pid " && -d "$LOG_DIR" ]]; then
                     echo "Detected new process with PID $pid for *$func_name*. Starting strace..."
                     # strace 실행
                     start_strace $pid "$func_name"
-                    # traced_pids add
                     traced_pids+=("$pid")
                 fi
             fi
@@ -104,8 +125,8 @@ mount_debugfs
 # TCPDump 시작
 start_tcpdump
 
-#프로세스 감시 함수 호출
-monitor_processes
+# 프로세스 감시 함수 호출
+monitor_processes &
 
 # 무한 루프를 돌며 프로세스 상태를 감시
 while true; do
@@ -115,4 +136,3 @@ while true; do
     fi
     sleep 10
 done
-
